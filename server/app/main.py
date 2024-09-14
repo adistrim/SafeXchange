54014ef65dfca4c943eee3a97aded5eac3f83efb
Aditya Raj <adistrim.dev@gmail.com>
2024-09-14 14:54:21 +0530

    added CORSMiddleware

--- server/app/main.py
+++ server/app/main.py
@@ -1,8 +1,25 @@
 from fastapi import FastAPI
+from starlette.middleware.cors import CORSMiddleware
 from app.routers import auth
 from app.database import init_db
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
 
 app = FastAPI()
+
+origins = [
+    os.getenv("ORIGIN_URL")
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 @app.on_event("startup")
 async def startup_event():
